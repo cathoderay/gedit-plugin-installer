3d5f095956e1a779a8dcdc7ef883f973d7fca998
Ronald Andreu Kaiser <raios.catodicos@gmail.com>
2010-01-04 20:31:08 -0200

    setting up...showing on 'Preferences'...

---
+++ plugin_installer.py
@@ -0,0 +1,12 @@
+import gedit
+
+
+class PluginInstaller(gedit.Plugin):
+    def activate(self, window):
+        pass
+
+    def deactivate(self, window):
+        pass
+
+    def update_ui(self, window):
+        pass
