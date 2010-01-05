79229e797cbb67dbf3a65a546e8bb5c2e792394c
Ronald Andreu Kaiser <raios.catodicos@gmail.com>
2010-01-04 23:56:46 -0200

    Added a menu item

--- plugin_installer.py
+++ plugin_installer.py
@@ -1,12 +1,90 @@
+from gettext import gettext as _
+
+import gtk
 import gedit
 
 
+# Menu item example, insert a new item in the Tools menu
+ui_str = """<ui>
+  <menubar name="MenuBar">
+    <menu name="ToolsMenu" action="Tools">
+      <placeholder name="ToolsOps_2">
+        <menuitem name="ExamplePy" action="ExamplePy"/>
+      </placeholder>
+    </menu>
+  </menubar>
+</ui>
+"""
+
+
+class PluginInstallerWindowHelper:
+    def __init__(self, plugin, window):
+        self._window = window
+        self._plugin = plugin
+
+        # Insert menu items
+        self._insert_menu()
+
+    def deactivate(self):
+        # Remove any installed menu items
+        self._remove_menu()
+
+        self._window = None
+        self._plugin = None
+        self._action_group = None
+
+    def _insert_menu(self):
+        # Get the GtkUIManager
+        manager = self._window.get_ui_manager()
+
+        # Create a new action group
+        self._action_group = gtk.ActionGroup("ExamplePyPluginActions")
+        self._action_group.add_actions([("ExamplePy", None, _("Install plugin..."),
+                                         None, _("Clear the document"),
+                                         self.on_install_plugin_activate)])
+
+        # Insert the action group
+        manager.insert_action_group(self._action_group, -1)
+
+        # Merge the UI
+        self._ui_id = manager.add_ui_from_string(ui_str)
+
+    def _remove_menu(self):
+        # Get the GtkUIManager
+        manager = self._window.get_ui_manager()
+
+        # Remove the ui
+        manager.remove_ui(self._ui_id)
+
+        # Remove the action group
+        manager.remove_action_group(self._action_group)
+
+        # Make sure the manager updates
+        manager.ensure_update()
+
+    def update_ui(self):
+        self._action_group.set_sensitive(self._window.get_active_document() != None)
+
+    # Menu activate handlers
+    def on_install_plugin_activate(self, action):
+        d = gtk.Dialog("Work in progress...")
+        d.show()
+
+    def update_ui(self):
+        pass    
+
+
 class PluginInstaller(gedit.Plugin):
+    def __init__(self):
+        gedit.Plugin.__init__(self)
+        self._instances = {}
+
     def activate(self, window):
-        pass
+        self._instances[window] = PluginInstallerWindowHelper(self, window)
 
     def deactivate(self, window):
-        pass
+        self._instances[window].deactivate()
+        del self._instances[window]
 
     def update_ui(self, window):
-        pass
+        self._instances[window].update_ui()
